56b44ba71a66d750266a6eef8963c66ef318f565
maac410 <maac410@gmail.com>
2024-12-30 15:57:47 -0500

    #

--- BT/Class1.cs
+++ BT/Class1.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.Revit.UI;
+﻿using Autodesk.Revit.UI;
 using Autodesk.Revit.DB;
+using System.Runtime.InteropServices;
 using System.Reflection;
+using System;
 
-
-namespace BT
+namespace Bt_First
 {
     public class Class1 : IExternalApplication
     {
-        public Result OnShutdown(UIControlledApplication application) { 
-            throw new NotImplementedException();
-        }
-
         public Result OnStartup(UIControlledApplication application)
         {
             RibbonPanel ribbonPanel = application.CreateRibbonPanel("My Ribbon Panel");
             string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
 
-            PushButtonData buttonData = new PushButtonData("cmdMyTest", "My Test", thisAssemblyPath, "BT.MyTest");
-            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+            PushButtonData buttonData = new PushButtonData("cmdMyTest", "My Test", thisAssemblyPath, "Bt_First.MyTest");
+            PushButton? pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
             pushButton.ToolTip = "Hello World";
 
             return Result.Succeeded;
@@ -30,11 +22,13 @@ namespace BT
 
         public class MyTest : IExternalCommand
         {
+
             public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
             {
                 var uiapp = commandData.Application;
                 var uidoc = uiapp.ActiveUIDocument;
 
+                // Show a simple dialog
                 TaskDialog.Show("Revit", "Hello World");
 
                 return Result.Succeeded;
@@ -42,4 +36,3 @@ namespace BT
             }
         }
     }
-}
