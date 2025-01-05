e8115403fedbbff8aa6d80cde787d3ca824814d0
maac410 <maac410@gmail.com>
2025-01-05 11:53:27 -0500

    working

--- BT/Class1.cs
+++ BT/Class1.cs
@@ -3,36 +3,139 @@ using Autodesk.Revit.DB;
 using System.Runtime.InteropServices;
 using System.Reflection;
 using System;
+using Autodesk.Revit.Attributes;
+using System.Collections.Generic;
+using Autodesk.Revit.UI.Selection;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Parameter = Autodesk.Revit.DB.Parameter;
+using System.Linq;
 
-namespace Bt_First
+namespace BT
 {
     public class Class1 : IExternalApplication
     {
-        public Result OnStartup(UIControlledApplication application)
+        public Result OnShutdown(UIControlledApplication application)
         {
-            RibbonPanel ribbonPanel = application.CreateRibbonPanel("My Ribbon Panel");
-            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
-
-            PushButtonData buttonData = new PushButtonData("cmdMyTest", "My Test", thisAssemblyPath, "Bt_First.MyTest");
-            PushButton? pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
-            pushButton.ToolTip = "Hello World";
-
+            // Implement shutdown logic if needed
             return Result.Succeeded;
         }
 
-        public class MyTest : IExternalCommand
+        public Result OnStartup(UIControlledApplication application)
         {
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("BT");
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
 
-            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            PushButtonData buttonData = new PushButtonData("cmdMyTest", "My Test", thisAssemblyPath, "BT.MyTest");
+            PushButton pushButton = ribbonPanel.AddItem(buttonData) as PushButton;
+            pushButton.ToolTip = "Create an Assembly Blueprint";
+
+            return Result.Succeeded;
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    public class MyTest : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var uiapp = commandData.Application;
+            var uidoc = uiapp.ActiveUIDocument;
+            var doc = uidoc.Document;
+
+            string info = "Selected element IDs are: ";
+            bool? hasAssembly = null;
+
+            Autodesk.Revit.UI.Selection.Selection selection = uidoc.Selection;
+
+            try
             {
-                var uiapp = commandData.Application;
-                var uidoc = uiapp.ActiveUIDocument;
+                ICollection<ElementId> selectedIds = selection.GetElementIds();
 
-                // Show a simple dialog
-                TaskDialog.Show("Revit", "Hello World");
+                if (selectedIds.Count == 0)
+                {
+                    TaskDialog.Show("Revit", "You haven't selected any elements.");
+                    return Autodesk.Revit.UI.Result.Failed;
+                }
 
-                return Result.Succeeded;
-                throw new NotImplementedException();
+                // A HashSet to store processed category names or element type names
+                HashSet<string> processedCategories = new HashSet<string>();
+
+                foreach (ElementId id in selectedIds)
+                {
+                    Element element = doc.GetElement(id);
+
+                    if (element is AssemblyInstance assemblyInstance)
+                    {
+                        hasAssembly = true;
+                        info += $"\n\tAssembly Instance ID: {id.IntegerValue}";
+
+                        // Process the assembly's member elements
+                        ICollection<ElementId> memberIds = assemblyInstance.GetMemberIds();
+
+                        foreach (ElementId memberId in memberIds)
+                        {
+                            Element member = doc.GetElement(memberId);
+                            info += $"\n\tAssembly member Instance ID: {memberId.IntegerValue}";
+
+                            // Get the ElementType (or FamilyType) of the member
+                            ElementType memberType = doc.GetElement(member.GetTypeId()) as ElementType;
+                            if (memberType != null)
+                            {
+                                // Get the category name or type name (this is what will be tracked)
+                                string categoryName = member.Category.Name;
+                                ParameterSet parameters = member.Parameters;
+
+                                // If this category hasn't been processed yet, add it to the list
+                                if (!processedCategories.Contains(categoryName))
+                                {
+                                    processedCategories.Add(categoryName);
+                                }
+
+                                // Get parameters of the member's type (element type)
+                                Parameter[] typeParameters = memberType.Parameters.Cast<Parameter>().ToArray();
+
+                                // Collect information about the parameters of the type
+                                foreach (Parameter param in typeParameters)
+                                {
+                                    string paramName = param.Definition.Name;
+                                    string paramValue = param.AsString() ?? param.AsValueString() ?? "No value";
+
+                                    // Add parameter details to the info string
+                                    info += $"\n\tCategory: {categoryName}, Type Parameter: {paramName}, Value: {paramValue}";
+                                }
+                            }
+                            else
+                            {
+                                info += $"\n\tMember ID: {memberId.IntegerValue}, No ElementType found.";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        TaskDialog.Show("Revit", "One of the selected elements is not an assembly instance. Execution will stop.");
+                        return Autodesk.Revit.UI.Result.Failed;
+                    }
+                }
+
+                // Show the result if assembly instances were found
+                if (hasAssembly.HasValue && hasAssembly.Value && selectedIds.Count < 2)
+                {
+                    // Show info in a custom Windows Form (SimpleForm)
+                    var simpleForm = new SimpleForm(info, processedCategories);  // Pass categories to the form
+                    simpleForm.ShowDialog();  // Show the form
+                }
+                else
+                {
+                    TaskDialog.Show("Revit", "No assembly instance elements selected or an item that isn't an assembly selected or there is more than one assembly selected.");
+                }
+
+                return Autodesk.Revit.UI.Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Autodesk.Revit.UI.Result.Failed;
             }
         }
+    }
     }
---
+++ BT/SimpleForm.xaml.cs
@@ -0,0 +1,131 @@
+﻿using BT;
+using DocumentFormat.OpenXml.EMMA;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace BT
+{
+    public partial class SimpleForm : Window
+    {
+        public SimpleForm()
+        {
+            InitializeComponent();
+        }
+
+        // Constructor that accepts info and processed categories (HashSet)
+        public SimpleForm(string info, HashSet<string> processedCategories) : this()
+        {
+            // Display the provided info in the ListBox
+            DisplayInfo(info, "Box");
+            
+
+            // Dynamically create and add checkboxes for each category to mBox
+            foreach (var category in processedCategories)
+            {
+                // Create a new CheckBox
+                CheckBox checkBox = new CheckBox();
+                checkBox.Content = category; // Set the checkbox content as the category name
+                checkBox.Margin = new Thickness(5);
+
+                // Optionally add an event handler for Checked/Unchecked events
+                checkBox.Checked += CheckBox_Checked;
+                checkBox.Unchecked += CheckBox_Unchecked;
+
+                // Wrap the checkbox in a ListBoxItem (mBox requires ListBoxItems)
+                ListBoxItem listBoxItem = new ListBoxItem();
+                listBoxItem.Content = checkBox;
+
+                // Add the ListBoxItem (containing the CheckBox) to mBox
+                mBox.Items.Add(listBoxItem);
+            }
+        }
+
+        // Show the form with the passed info and specify which ListBox to use
+        public void ShowDialog(string info, string listBoxName)
+        {
+            // Display info in the specified ListBox
+            DisplayInfo(info, listBoxName);
+
+            // Show the dialog window
+            this.ShowDialog();
+        }
+
+        // Helper method to handle displaying information in a ListBox
+        private void DisplayInfo(string info, string listBoxName)
+        {
+            // Split the info string into an array of lines
+            var formattedCollection = info.Split(new[] { "\n", "\r\n" }, StringSplitOptions.None);
+
+            // Clear the existing items from the ListBox
+            if (listBoxName == "Box")
+            {
+                Box.Items.Clear(); // Clear the existing items from Box
+                Box.ItemsSource = formattedCollection; // Set the new ItemsSource
+            }
+            else if (listBoxName == "mBox")
+            {
+                mBox.Items.Clear(); // Clear the existing items from mBox
+                mBox.ItemsSource = formattedCollection; // Set the new ItemsSource
+            }
+        }
+
+
+        // Checkbox Checked event handler (optional)
+        private HashSet<string> selectedCategories = new HashSet<string>();
+
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // Cast sender to CheckBox
+            CheckBox checkBox = sender as CheckBox;
+
+            if (checkBox != null)
+            {
+                // Get the category name from the checkbox content
+                string category = checkBox.Content.ToString();
+
+                // Add the category to the selectedCategories HashSet
+                selectedCategories.Add(category);
+
+                // Optionally, update the info string with the selected category (if needed)
+                ShowDialog($"Categories selected: {category}", "mBox");
+            }
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // Cast sender to CheckBox
+            CheckBox checkBox = sender as CheckBox;
+
+            if (checkBox != null)
+            {
+                // Get the category name from the checkbox content
+                string category = checkBox.Content.ToString();
+
+                // Remove the category from the selectedCategories HashSet
+                selectedCategories.Remove(category);
+
+                // Optionally, update the info string to reflect the deselection
+                ShowDialog($"\nCategory deselected: {category}", "mBox");
+            }
+        }
+
+        // Cancel Button Click event handler
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+
+
+//var simpleForm = new SimpleForm(info); 
+//simpleForm.ShowDialogWithInfo(info, "mBox");
+
+//var simpleForm = new SimpleForm(info);
+//simpleForm.ShowDialogWithInfo(info, "box");
